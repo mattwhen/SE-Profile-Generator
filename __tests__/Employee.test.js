aa5c3432e9ba872cc99e6121cdf45a01902044c9
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2023-02-21 06:56:38 -0600

    created getter methods for the Employee class

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -15,6 +15,29 @@ getEmail()
 
 getRole() // Returns 'Employee'
 */ 
+class Employee {
+    constructor(name, id, email) {
+        this._name = name;
+        this._id = id; 
+        this._email = email;
+    }
+
+    // Getter methods
+    get getName() {
+        return this._name;
+    }
+    get getId() {
+        return this._id;
+    }
+    get getEmail() {
+        return this._email;
+    }
+}
+// JS calls the contructor method every time it creates a new instance/object of a class.
+const tom = new Employee('Tom', '15863', 'tom@facebook.com');
+console.log(tom.getName);
+console.log(tom.getId);
+console.log(tom.getEmail);
 // The other 3 classes (Engineer, Intern, Manager) will be extended from the Employee class.
 
 /* The describe method is a jest method for containg one or more tests.
@@ -22,10 +45,10 @@ it takes two arguments: a string for describing the test
 suite, and a callback function for wrapping the actual test */ 
 // Remember, testing is a matter of INPUTS, FUNCTIONS, and expected OUTPUTS. 
 
-// Write the test block: 
-describe('', () => { 
-    it('*Here is the test requirement*', () => {
-        //actual test goes here.
+// // Write the test block: 
+// describe('*Describe the test here*', () => { 
+//     it('*Here is the test requirement*', () => {
+//         //actual test goes here.
         
-    });
-});
+//     });
+// });
